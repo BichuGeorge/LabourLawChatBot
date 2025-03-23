be5378833dd46504603eb8d6614f3ec35049b7e7
Bichu George <99749851+BichuGeorge@users.noreply.github.com>
2025-03-23 23:48:55 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Generative AI project",
+    version= "0.0.0",
+    author="Bichu George",
+    author_email="bichugeo@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
